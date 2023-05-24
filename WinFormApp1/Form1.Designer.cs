33a4e43dc3439fd743adbe142719c8b0b3e13cc6
BYMCS <squalmoney@gmail.com>
2023-05-24 13:34:16 +0300

    DB APP and CBS

--- WinFormApp1/Form1.Designer.cs
+++ WinFormApp1/Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btnRead = new Button();
+            richTextBox1 = new RichTextBox();
+            richTextBox2 = new RichTextBox();
+            btnRead2 = new Button();
+            SuspendLayout();
+            // 
+            // btnRead
+            // 
+            btnRead.Location = new Point(12, 32);
+            btnRead.Name = "btnRead";
+            btnRead.Size = new Size(433, 35);
+            btnRead.TabIndex = 0;
+            btnRead.Text = "Read DB and Write";
+            btnRead.UseVisualStyleBackColor = true;
+            btnRead.Click += btnRead_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 87);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(433, 308);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Location = new Point(477, 87);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(424, 308);
+            richTextBox2.TabIndex = 3;
+            richTextBox2.Text = "";
+            // 
+            // btnRead2
+            // 
+            btnRead2.Location = new Point(477, 32);
+            btnRead2.Name = "btnRead2";
+            btnRead2.Size = new Size(424, 35);
+            btnRead2.TabIndex = 4;
+            btnRead2.Text = "Read Excel and Write";
+            btnRead2.UseVisualStyleBackColor = true;
+            btnRead2.Click += btnRead2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(917, 405);
+            Controls.Add(btnRead2);
+            Controls.Add(richTextBox2);
+            Controls.Add(richTextBox1);
+            Controls.Add(btnRead);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "DB READ and WRITE APP";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnRead;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private Button btnRead2;
     }
 }
--- WinFormApp1/Form1.cs
+++ WinFormApp1/Form1.cs
@@ -1,10 +1,166 @@
+using Microsoft.Office.Interop.Excel;
+using System.Collections;
+using System.Data.SqlClient;
+using Excel = Microsoft.Office.Interop.Excel;
+
 namespace WinFormApp1
 {
     public partial class Form1 : Form
     {
+        readonly SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ProjelerVT;Integrated Security=True");
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = true;
+            Excel.Workbook workbook = excelApp.Workbooks.Add(System.Reflection.Missing.Value);
+            Excel.Worksheet worksheet = workbook.Sheets[1];
+
+            string[] basliklar = { "Personel No", "Ad", "Soyad", "Semt", "Þehir" };
+            Excel.Range range;
+            for (int i = 0; i < basliklar.Length; i++)
+            {
+                range = worksheet.Cells[1, (1 + i)];
+                range.Value2 = basliklar[i];
+            }
+
+            try
+            {
+                con.Open();
+                string sql = "SELECT PersonelNo, Ad, Soyad, Semt, Sehir FROM Personel";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                int satir = 2;
+
+                while (reader.Read())
+                {
+                    string? pNo = reader[0].ToString();
+                    string? ad = reader[1].ToString();
+                    string? soyad = reader[2].ToString();
+                    string? semt = reader[3].ToString();
+                    string? sehir = reader[4].ToString();
+
+                    richTextBox1.Text = richTextBox1.Text + pNo + " " + ad + " " + soyad + " " + semt + " " + sehir + "\n";
+
+                    range = worksheet.Cells[satir, 1];
+                    range.Value2 = pNo;
+                    range = worksheet.Cells[satir, 2];
+                    range.Value2 = ad;
+                    range = worksheet.Cells[satir, 3];
+                    range.Value2 = soyad;
+                    range = worksheet.Cells[satir, 4];
+                    range.Value2 = semt;
+                    range = worksheet.Cells[satir, 5];
+                    range.Value2 = sehir;
+
+                    satir++;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+
+
+        }
+
+
+        private void btnRead2_Click(object sender, EventArgs e)
+        {
+            Excel.Application exApp;
+            Excel.Workbook exWorkbook;
+            Excel.Worksheet exWorksheet;
+            Excel.Range range;
+            int rowCount = 0;
+            int columnCount = 0;
+
+            exApp = new Excel.Application();
+            exWorkbook = exApp.Workbooks.Open(@"C:\Users\mc.sahin\Documents\test2.xlsx");
+            exWorksheet = (Excel.Worksheet)exWorkbook.Worksheets.get_Item(1);
+            range = exWorksheet.UsedRange;
+            richTextBox2.Clear();
+
+            // ilk satýr baþlýklarý içerdiði için rowCount'u 2'den baþlatmamýz gerekiyor. 
+            // Eðer ilk satýrda veriler baþlamýþ olsaydý 1'den baþlatmamýz gerekirdi.
+
+            //MessageBox.Show(range.Rows.Count + " // " + range.Columns.Count);
+
+
+            for (rowCount = 2; rowCount <= range.Rows.Count; rowCount++)
+            {
+                ArrayList list = new ArrayList();
+
+                for (columnCount = 1; columnCount <= range.Columns.Count; columnCount++)
+                {
+
+                    string okunanHucre = Convert.ToString((range.Cells[rowCount, columnCount] as Excel.Range).Value2);
+                    richTextBox2.Text = richTextBox2.Text + okunanHucre + " ";
+                    list.Add(okunanHucre);
+                }
+                richTextBox2.Text = richTextBox2.Text + "\n";
+
+                try
+                {
+                    con.Open();
+                    SqlCommand command = new SqlCommand("INSERT INTO Personel (PersonelNo,Ad,Soyad,Semt,Sehir)" +
+                                                        "VALUES (@P1, @P2, @P3, @P4, @P5)", con);
+
+                    command.Parameters.AddWithValue("@P1", list[0]);
+                    command.Parameters.AddWithValue("@P2", list[1]);
+                    command.Parameters.AddWithValue("@P3", list[2]);
+                    command.Parameters.AddWithValue("@P4", list[3]);
+                    command.Parameters.AddWithValue("@P5", list[4]);
+                    command.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    if (con != null) con.Close();
+                }
+
+
+
+            }
+
+            exApp.Quit();
+            ReleaseObject(exWorksheet);
+            ReleaseObject(exWorkbook);
+            ReleaseObject(exApp);
+
+        }
+
+        private void ReleaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+
         }
     }
 }
